802a6e432c3f5751bfe6c1ce17df4b78959191df
Julen <badiolajulen@opendeusto.es>
2020-04-16 18:22:34 +0200

    Google login and log out functional. Changes in templates.

--- BSPQ20E3/BSPQ20E3/settings.py
+++ BSPQ20E3/BSPQ20E3/settings.py
@@ -56,7 +56,7 @@ AUTHENTICATION_BACKENDS = (
 
 LOGIN_URL = '/auth/login/google-oauth2/'
 
-LOGIN_REDIRECT_URL = '/manage/'
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 SOCIAL_AUTH_URL_NAMESPACE = 'social'
 
