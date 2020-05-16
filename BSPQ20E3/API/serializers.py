a7a7bfa9df9aeca4d604d595129ecbb2e6fc71a1
Julen <badiolajulen@opendeusto.es>
2020-05-16 18:24:57 +0200

    API app documentation

--- BSPQ20E3/API/serializers.py
+++ BSPQ20E3/API/serializers.py
@@ -2,6 +2,14 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
+	'''Serializes user objects
+
+	:param req: User object queryset
+	:type amount: User object
+
+	:returns: Users list
+	:rtype: Json
+	'''
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'last_login')
--- BSPQ20E3/API/views.py
+++ BSPQ20E3/API/views.py
@@ -5,9 +5,16 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
-#https://www.django-rest-framework.org/api-guide/permissions/
 @permission_classes([IsAdminUser])
 class UserViewSet(viewsets.ModelViewSet):
-	""" API endpoint that allows users to be viewed or edited."""
+	'''API endpoint that allows users to be viewed or edited.
+	Only accesible by admins
+
+	:param req: The Http Request
+	:type amount: Http Request
+
+	:returns: Http Response
+	:rtype: Http
+	'''
 	queryset = User.objects.all().order_by('-date_joined')
 	serializer_class = UserSerializer
